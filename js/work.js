831ab4de4da217e44a37b36a2829201a6f1c62e1
Abi Karunaratne <59220473+Szask1a@users.noreply.github.com>
2021-04-12 01:06:45 +0530

    Update work.js

--- js/work.js
+++ js/work.js
@@ -59,7 +59,7 @@
         cssClass: 'ms--links',
         range: [0, 5],
         rangeContent: function (i) {
-            return 'links[i]';
+            return '<a class="ms-slide__link" href="https://www.behance.net/gallery/102425127/Bliss">View Case</a>';
         },
         vertical: true,
         interactive: false
