d283730cab9fe63331733762a2b20c7253647be0
Abi Karunaratne <59220473+Szask1a@users.noreply.github.com>
2021-04-12 00:02:54 +0530

    Update work.js

--- js/work.js
+++ js/work.js
@@ -59,7 +59,7 @@
         cssClass: 'ms--links',
         range: [0, 5],
         rangeContent: function () {
-            return '<a class="ms-slide__link" href="+links[]+">View Case</a>';
+            return '<a class="ms-slide__link" href=""+links[]+>View Case</a>';
         },
         vertical: true,
         interactive: false
